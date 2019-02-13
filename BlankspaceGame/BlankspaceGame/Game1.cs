201eb80ce3c715efebce7d053b8217e2888321d9
irm8232 <irm8232@IGMOH1375-28>
2019-02-13 11:45:54 -0500

    Edited the comment on line 8

--- BlankspaceGame/BlankspaceGame/Game1.cs
+++ BlankspaceGame/BlankspaceGame/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace BlankspaceGame
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// This is IKE MCNUTT reporting for duty
     /// </summary>
     public class Game1 : Game
     {
