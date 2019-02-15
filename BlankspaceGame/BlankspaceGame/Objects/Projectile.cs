2791bf057134d291c34b18cfd2484684b97074b7
irm8232 <irm8232@IGM2025-03>
2019-02-14 20:27:54 -0500

    Setup the consturctor and fields for Projectile

--- BlankspaceGame/BlankspaceGame/Objects/Projectile.cs
+++ BlankspaceGame/BlankspaceGame/Objects/Projectile.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace BlankspaceGame
 {
-    class Projectile
+    class Projectile : GameObject
     {
+        // Velocity, speed and damage fields
+        private Vector2 velocity;
+        private int speed;
+        private int damage;
+
+        public Projectile(Vector2 unitVelocity, int speed, Rectangle rect, Texture2D text) : base(rect, text)
+        {
+            this.velocity = unitVelocity;
+            this.speed = speed;
+        }
     }
 }
