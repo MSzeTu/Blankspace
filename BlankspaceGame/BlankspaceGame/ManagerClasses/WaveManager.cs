8a2fda4f9920b41a817b85e69b63ac21d61cfb84
irm8232 <irm8232@IGM2000-30>
2019-04-24 20:55:14 -0400

    Added loading all levels withing the directory

--- BlankspaceGame/BlankspaceGame/ManagerClasses/WaveManager.cs
+++ BlankspaceGame/BlankspaceGame/ManagerClasses/WaveManager.cs
@@ -25,7 +25,7 @@ namespace BlankspaceGame
         {
             ".\\Content\\Levels\\Level1.wave",
             ".\\Content\\Levels\\level2.wave",
-            //".\\Content\\Levels\\level3.wave",
+            ".\\Content\\Levels\\level3.wave",
             //".\\Content\\Levels\\ouch.wave"
 
         };
@@ -87,8 +87,9 @@ namespace BlankspaceGame
             currentWave = 0;
             currentTime = 0;
             currentLevel = 0;
+            levelsToLoad = GetLevelsInFolder();
 
-            for (int i = 0; i < LevelCount; i++)
+            for (int i = 0; i < levelsToLoad.Length; i++)
             {
                 LoadWaves(levelsToLoad[i]);
             }
@@ -193,6 +194,34 @@ namespace BlankspaceGame
             currentWave = 0;
             currentLevel = 0;
             toNextLevel = false;
+        }
+
+        static string[] GetLevelsInFolder()
+        {
+
+            // Check if the levels folder exists
+            if (Directory.Exists(".\\Content\\Levels\\"))
+            {
+                // Get the directory
+                DirectoryInfo levelsFolder = new DirectoryInfo(".\\Content\\Levels\\");
+
+                // Get the level files
+                FileInfo[] files = levelsFolder.GetFiles();
+
+                // Get the length of the level name string
+                int levelCount = files.Length;
+                string[] lvlsToLoad = new string[levelCount];
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    lvlsToLoad[i] = $".\\Content\\Levels\\{files[i].Name}";
+                }
+
+                return lvlsToLoad;
+            } else
+            {
+                throw new Exception("The level folder does not exist!");
+            }
         }
     }
 }
